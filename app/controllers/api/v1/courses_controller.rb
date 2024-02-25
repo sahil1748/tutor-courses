5bfab4ceb94832d753e2b8ba20880d997d919a4d
Sahil Jha <sahil1748@gmail.com>
2024-02-25 16:36:36 +0530

    Add courses post api | course creator service

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -1,8 +1,47 @@
 class Api::V1::CoursesController < Api::V1::BaseController
 
 	def create
+		res = CourseCreator.new(course_params, tutors, tutor_ids)
+					.process
+
+		if res[:error_messages].present?
+			render json: {
+											success: false,
+											error_messages: res[:error_messages]
+										},
+										status: :unprocessable_entity
+		else
+    	render json: { 
+							    		success: true,
+							    		course: res[:course]
+							    	},
+							    	status: :created
+		end
 	end
 
 	def index
-  end
+	end
+
+	private
+
+	def course_params
+		params.require(:course).permit(
+			:name, :description,
+			:category, :level
+		)
+	end
+
+	def tutor_ids
+		params.permit(tutor_ids: [])
+	end
+
+	def tutors
+		params.permit(tutors: [
+														:first_name,
+													 	:last_name,
+														:date_of_birth,
+														:primary_skill,
+														:about
+													])
+	end
 end
---
+++ app/services/CourseCreator.rb
@@ -0,0 +1,32 @@
+class CourseCreator
+	def initialize(course={}, tutors=[], tutor_ids=[])
+		@course = course
+		@tutors = tutors
+		@tutor_ids = tutor_ids
+		@error_messages = {}
+	end
+
+	def process
+		Course.transaction do
+			@course_rec = Course.new(@course)
+			if @course_rec.save
+				assign_tutors if @tutors.present?
+			else
+				@error_messages.merge! @course_rec.errors.messages
+			end
+		end
+
+		{error_messages: @error_messages, course: @course_rec}
+	end
+
+	def assign_tutors
+		result = create_tutors
+		success_ids = result.pluck(:id).compact
+
+		Tutor.where(id: success_ids + @tutor_ids).update_all(course_id: @course_rec.id)
+	end
+
+	def create_tutors
+		Tutor.create(@tutors[:tutors])
+	end
+end
